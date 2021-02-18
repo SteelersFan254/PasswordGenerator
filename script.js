a1fa6c2e793c49b99a3de43de9be4aed41e95c24
Ryan Borja <rcborja.school@gmail.com>
2021-02-18 13:33:55 -0600

    added getCharChoice function to return the possible characters being used based off of the users choice

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
 var genBtn = document.getElementById("createPassword");
 var lower = document.getElementById("lower");
+var upper = document.getElementById("upper");
+var numbers = document.getElementById("numbers");
+var special = document.getElementById("special");
+var slider = document.getElementById("slider");
+var password = "";
+var lowerChars = "abcdefghijklmnopqrstuvwxyz";
+var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numChars = "0123456789";
+var specialChars = "!@#$%^&*()_+=-{}|[]:;'<>?,./";
+var possibleChars = "";
+var newPassword = document.getElementById("generatedPassword")
 
-genBtn.addEventListener("click", function () {
-    if (lower.checked === true) {
-        console.log("lower")
-    } else {
-        console.log("not owrking")
+function getCharChoice() {
+    if (possibleChars !== "") {
+        possibleChars = "";
     }
+    if (lower.checked === true) {
+        possibleChars += lowerChars;
+        console.log(possibleChars)
+    };
+    if (upper.checked === true) {
+        possibleChars += upperChars;
+        console.log(possibleChars)
+    };
+    if (numbers.checked === true) {
+        possibleChars += numChars;
+        console.log(possibleChars)
+    };
+    if (special.checked === true) {
+        possibleChars += specialChars;
+        console.log(possibleChars)
+    };
+    if (possibleChars === "") {
+        console.log("no characters selected")
+    }
+    return possibleChars;
+}
+
+function generatePassword(chars) {
+    var genPassword = "";
+}
+genBtn.addEventListener("click", function () {
+    getCharChoice();
 })
+console.log("is working " + possibleChars);
 
 function outputUpdate(vol) {
-	document.querySelector('#volume').value = vol;
+	document.querySelector('#currentCharCount').value = vol;
 }
+
